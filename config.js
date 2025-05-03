49698814bc76f5c886d39a5a6b61cade3fd75d06
MeG <112762788+MeG0302@users.noreply.github.com>
2025-05-03 23:43:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,32 @@
-// config.js
 export const CHAINS = {
   SEPOLIA: 11155111,
-  CORN: 21000001
+  HOLESKY: 17000,
+  BABYLON: 'babylon-testnet',
+  XION: 'xion-testnet-1',
+  CORN: 99999 // Confirm actual ID
 };
 
 export const RPC_URLS = {
   SEPOLIA: 'https://rpc.sepolia.org',
+  HOLESKY: 'https://rpc.holesky.ethpandaops.io',
+  BABYLON: 'https://babylon-testnet-rpc.polkachu.com',
+  XION: 'https://xion-testnet-rpc.polkachu.com',
   CORN: 'https://rpc.ankr.com/corn_testnet'
 };
 
-export const UNION_CONTRACT = {
-  SEPOLIA: '0xUnionV2SepoliaContractAddress' // Replace with actual
+export const TOKENS = {
+  WETH: {
+    SEPOLIA: '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9',
+    HOLESKY: '0xHoleskyWETHAddress'
+  },
+  USDC: {
+    SEPOLIA: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
+    XION: 'xion1usdcaddress'
+  }
 };
 
-// Add this new export:
-export const TOKEN_ADDRESSES = {
-  SEPOLIA_WETH: '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9' // Sepolia WETH
+export const UNION_CONTRACT = {
+  SEPOLIA: '0xUnionSepoliaAddr',
+  HOLESKY: '0xUnionHoleskyAddr',
+  // Add other chains
 };
