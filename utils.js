e02b75f16463754fcc77185734f299963c6fa7aa
MeG <112762788+MeG0302@users.noreply.github.com>
2025-05-03 22:53:15 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,30 +1,13 @@
-const { ethers } = require('ethers');
-const { CHAINS, UNION_CONTRACT, RPC_URLS } = require('./config');
+import { ethers } from 'ethers';
+import { CHAINS, UNION_CONTRACT, RPC_URLS } from './config.js'; // Note .js extension
 
 // Initialize provider and wallet
-const getProvider = (chainId) => {
+export const getProvider = (chainId) => {
   const rpcUrl = RPC_URLS[chainId];
   return new ethers.JsonRpcProvider(rpcUrl);
 };
 
 // Generic token transfer function
-const sendToken = async ({ sourceChain, destChain, asset, amount, privateKey }) => {
+export const sendToken = async ({ sourceChain, destChain, asset, amount, privateKey }) => {
   const provider = getProvider(sourceChain);
   const wallet = new ethers.Wallet(privateKey, provider);
-  
-  const unionContract = new ethers.Contract(
-    UNION_CONTRACT[sourceChain],
-    ['function transfer(uint16 destChainId, address asset, uint256 amount)'],
-    wallet
-  );
-
-  const tx = await unionContract.transfer(
-    CHAINS[destChain],
-    asset,
-    ethers.parseUnits(amount.toString(), 18) // Adjust decimals
-  );
-  
-  return tx.hash;
-};
-
-module.exports = { getProvider, sendToken };
